89f2fd383365eff6ef2c49feaf642df51568a11a
Lsy <379338943@qq.com>
2024-06-20 16:38:33 +0800

    初始化YIUI Demo

--- Scripts/HotfixView/Client/EntryEvent3_InitClient.cs
+++ Scripts/HotfixView/Client/EntryEvent3_InitClient.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using YIUIFramework;
 
 namespace ET.Client
 {
@@ -15,6 +16,18 @@ namespace ET.Client
             root.AddComponent<ResourcesLoaderComponent>();
             root.AddComponent<PlayerComponent>();
             root.AddComponent<CurrentScenesComponent>();
+            
+            {
+                //YIUI初始化
+                YIUIBindHelper.InternalGameGetUIBindVoFunc = YIUICodeGenerated.YIUIBindProvider.Get;
+                await root.AddComponent<YIUIMgrComponent>().Initialize();
+
+                //根据需求自行处理 在editor下自动打开  也可以根据各种外围配置 或者 GM等级打开
+                //if (Define.IsEditor) //这里默认都打开
+                {
+                    root.AddComponent<GMCommandComponent>();
+                }
+            }
             
             await EventSystem.Instance.PublishAsync(root, new AppStartInitFinish());
         }
