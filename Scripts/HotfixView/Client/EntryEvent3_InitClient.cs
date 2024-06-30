3283cb0a0a43e941ad73d3d3d21485292039853b
Lsy <379338943@qq.com>
2024-06-30 14:49:18 +0800

    增加GM命令

--- Scripts/HotfixView/Client/EntryEvent3_InitClient.cs
+++ Scripts/HotfixView/Client/EntryEvent3_InitClient.cs
@@ -23,7 +23,7 @@ namespace ET.Client
                 //根据需求自行处理 在editor下自动打开  也可以根据各种外围配置 或者 GM等级打开
                 //if (Define.IsEditor) //这里默认都打开
                 {
-                    //root.AddComponent<GMCommandComponent>();
+                    root.AddComponent<GMCommandComponent>();
                 }
             }
 
