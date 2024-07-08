2740cea17670b4186d067812ea149a7f35125249
Lsy <379338943@qq.com>
2024-07-08 18:42:07 +0800

    优化调整YIUI启动流程

--- Scripts/HotfixView/Client/EntryEvent3_InitClient.cs
+++ Scripts/HotfixView/Client/EntryEvent3_InitClient.cs
@@ -6,7 +6,7 @@ using YIUIFramework;
 namespace ET.Client
 {
     [Event(SceneType.StateSync)]
-    public class EntryEvent3_InitClient: AEvent<Scene, EntryEvent3>
+    public class EntryEvent3_InitClient : AEvent<Scene, EntryEvent3>
     {
         protected override async ETTask Run(Scene root, EntryEvent3 args)
         {
@@ -15,19 +15,11 @@ namespace ET.Client
             root.AddComponent<PlayerComponent>();
             root.AddComponent<CurrentScenesComponent>();
 
+            var result = await root.AddComponent<YIUIMgrComponent>().Initialize();
+            if (!result)
             {
-                var result = await root.AddComponent<YIUIMgrComponent>().Initialize();
-                if (!result)
-                {
-                    Log.Error("初始化UI失败");
-                    return;
-                }
-                //TODO 以后这些消息统一到YIUIMGR初始化成功后自行调用
-                //根据需求自行处理 在editor下自动打开  也可以根据各种外围配置 或者 GM等级打开
-                //if (Define.IsEditor) //这里默认都打开
-                {
-                    root.AddComponent<GMCommandComponent>();
-                }
+                Log.Error("初始化UI失败");
+                return;
             }
 
             await EventSystem.Instance.PublishAsync(root, new AppStartInitFinish());
