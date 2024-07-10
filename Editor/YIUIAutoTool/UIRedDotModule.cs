71497f39b6fad02f44ae8e7f1e9930a8dfd4f50f
Lsy <379338943@qq.com>
2024-07-10 20:25:53 +0800

    重构demo不用全量

---
+++ Editor/YIUIAutoTool/UIRedDotModule.cs
@@ -0,0 +1,36 @@
+﻿//------------------------------------------------------------
+// Author: 亦亦
+// Mail: 379338943@qq.com
+// Data: 2023年2月12日
+//------------------------------------------------------------
+
+#if UNITY_EDITOR
+using System;
+using System.IO;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+
+namespace YIUIFramework.Editor
+{
+    [YIUIAutoMenu("Demo", 1000000)]
+    internal class UIDemoModule : BaseYIUIToolModule
+    {
+        //当前所有脚本跟原始demo对比 然后吧他的都给注释了 用我的
+        
+        //YOOasset的设置 这边写一个配置 然后那边重新设置一下
+        
+        
+        
+        public override void Initialize()
+        {
+
+        }
+
+        public override void OnDestroy()
+        {
+
+        }
+    }
+}
+#endif
