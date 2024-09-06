be590a983dfe7c18a4803bcfb82af3e1f01f125c
Lsy <379338943@qq.com>
2024-09-06 18:44:26 +0800

    功能可独立使用

--- Editor/YIUIDemoWindow.cs
+++ Editor/YIUIDemoWindow.cs
@@ -45,8 +45,6 @@ namespace YIUIFramework.Editor
         {
             CloseWindow();
             AssetDatabase.SaveAssets();
-
-            //AssetDatabase.Refresh();//下面的刷新更NB
             EditorApplication.ExecuteMenuItem("Assets/Refresh");
         }
 
@@ -87,86 +85,16 @@ namespace YIUIFramework.Editor
             ScriptsReferencesHelper.Run();
         }
 
-        [BoxGroup("  ")]
-        [Button("设置TMP中文字体", 40)]
-        private void SetTMP()
+        [HorizontalGroup("DemoChange")]
+        [Button("同步YooAsset设置", 30)]
+        private void DemoSyncYooAssetSetting()
         {
-            var tmpSettings = Resources.Load<TMP_Settings>("TMP Settings");
-            if (tmpSettings == null)
+            if (SyncYooAssetSetting())
             {
-                Debug.LogError($"没有找到TMP设置 请手动设置字体");
-                return;
-            }
-
-            TMP_FontAsset newDefaultFontAsset = Resources.Load<TMP_FontAsset>("Fonts/SourceHanSansSC-VF SDF");
-            if (newDefaultFontAsset == null)
-            {
-                Debug.LogError($"没有找到新的默认字体 请检查");
-                return;
-            }
-
-            Type      settingsType          = typeof(TMP_Settings);
-            FieldInfo defaultFontAssetField = settingsType.GetField("m_defaultFontAsset", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            if (defaultFontAssetField != null && newDefaultFontAsset != null)
-            {
-                defaultFontAssetField.SetValue(tmpSettings, newDefaultFontAsset);
-            }
-
-            EditorUtility.DisplayDialog("提示", "设置TMP字体完毕", "确认");
-            CloseWindowRefresh();
-        }
-
-        [BoxGroup("   ")]
-        [Button("打开YooAsset设置", 40)]
-        private void SetYooAssetSetting()
-        {
-            Type      type      = typeof(AssetBundleCollectorSettingData);
-            FieldInfo fieldInfo = type.GetField("_setting", BindingFlags.NonPublic | BindingFlags.Static);
-            if (fieldInfo != null)
-            {
-                //根据Demo类型可以打开不同的YooAsset设置
-                if (OpenYIUI)
-                {
-                    var tempSetting = ScriptableObject.CreateInstance<AssetBundleCollectorSetting>();
-                    var yiuiSetting = SettingLoader.LoadSettingData<YIUIYooAssetSetting>();
-                    if (yiuiSetting == null)
-                    {
-                        Debug.LogError($"没有找到YIUIYooAssetSetting");
-                        return;
-                    }
-
-                    EditorUtility.SetDirty(yiuiSetting);
-                    tempSetting.Packages = yiuiSetting.Packages;
-                    fieldInfo.SetValue(null, tempSetting);
-                }
-                else
-                {
-                    fieldInfo.SetValue(null, null);
-                }
-
-                CloseWindowRefresh();
-                EditorApplication.ExecuteMenuItem("YooAsset/AssetBundle Collector");
-            }
-            else
-            {
-                Debug.LogError($"没有找到AssetBundleCollectorSettingData._setting");
+                EditorUtility.DisplayDialog("提示", "同步YooAsset设置 成功", "确认");
             }
         }
 
-        private void CreateUIProjectRes()
-        {
-            var path = $"{Application.dataPath}/../{UIProjectResPath}";
-
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-        }
-
-        /// <summary>
-        /// 同步YooAsset设置
-        /// </summary>
         private bool SyncYooAssetSetting()
         {
             var yooSetting = AssetBundleCollectorSettingData.Setting;
@@ -221,9 +149,16 @@ namespace YIUIFramework.Editor
             return true;
         }
 
-        /// <summary>
-        /// 场景切换
-        /// </summary>
+        [HorizontalGroup("DemoChange")]
+        [Button("场景切换", 30)]
+        private void DemoSwitchToScene()
+        {
+            if (SwitchToScene())
+            {
+                EditorUtility.DisplayDialog("提示", "场景切换 成功", "确认");
+            }
+        }
+
         private bool SwitchToScene()
         {
             var scenePath = $"Packages/cn.etetet.{(OpenYIUI ? YIUIPackageName : ETLoaderPackageName)}/Scenes/Init.unity";
@@ -238,9 +173,16 @@ namespace YIUIFramework.Editor
             return true;
         }
 
-        /// <summary>
-        /// 拷贝ET工程
-        /// </summary>
+        [HorizontalGroup("DemoChange")]
+        [Button("拷贝ET工程", 30)]
+        private void DemoCopyET()
+        {
+            if (CopyET())
+            {
+                EditorUtility.DisplayDialog("提示", "拷贝ET工程 成功", "确认");
+            }
+        }
+
         private bool CopyET()
         {
             var et                  = "ET.sln";
@@ -261,14 +203,22 @@ namespace YIUIFramework.Editor
             catch (Exception ex)
             {
                 Debug.LogError($"复制ET.sln过程中失败 注意请关闭IDE编辑器使用 {ex}");
+                //return false;
             }
 
             return true;
         }
 
-        /// <summary>
-        /// 打开关闭 -- 注释文件
-        /// </summary>
+        [HorizontalGroup("DemoChange")]
+        [Button("注释文件", 30)]
+        private void DemoChangeFile()
+        {
+            if (ChangeFile())
+            {
+                EditorUtility.DisplayDialog("提示", "注释文件 成功", "确认");
+            }
+        }
+
         private bool ChangeFile()
         {
             var sourceFilePath = $"{Application.dataPath}/../Packages/cn.etetet.{YIUIPackageName}/Scripts/HotfixView/Client";
@@ -350,6 +300,83 @@ namespace YIUIFramework.Editor
             }
 
             return true;
+        }
+
+        [BoxGroup("  ")]
+        [Button("设置TMP中文字体", 40)]
+        private void SetTMP()
+        {
+            var tmpSettings = Resources.Load<TMP_Settings>("TMP Settings");
+            if (tmpSettings == null)
+            {
+                Debug.LogError($"没有找到TMP设置 请手动设置字体");
+                return;
+            }
+
+            TMP_FontAsset newDefaultFontAsset = Resources.Load<TMP_FontAsset>("Fonts/SourceHanSansSC-VF SDF");
+            if (newDefaultFontAsset == null)
+            {
+                Debug.LogError($"没有找到新的默认字体 请检查");
+                return;
+            }
+
+            Type      settingsType          = typeof(TMP_Settings);
+            FieldInfo defaultFontAssetField = settingsType.GetField("m_defaultFontAsset", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (defaultFontAssetField != null && newDefaultFontAsset != null)
+            {
+                defaultFontAssetField.SetValue(tmpSettings, newDefaultFontAsset);
+            }
+
+            EditorUtility.DisplayDialog("提示", "设置TMP字体完毕", "确认");
+            CloseWindowRefresh();
+        }
+
+        [BoxGroup("   ")]
+        [Button("打开YooAsset设置", 40)]
+        private void SetYooAssetSetting()
+        {
+            Type      type      = typeof(AssetBundleCollectorSettingData);
+            FieldInfo fieldInfo = type.GetField("_setting", BindingFlags.NonPublic | BindingFlags.Static);
+            if (fieldInfo != null)
+            {
+                //根据Demo类型可以打开不同的YooAsset设置
+                if (OpenYIUI)
+                {
+                    var tempSetting = ScriptableObject.CreateInstance<AssetBundleCollectorSetting>();
+                    var yiuiSetting = SettingLoader.LoadSettingData<YIUIYooAssetSetting>();
+                    if (yiuiSetting == null)
+                    {
+                        Debug.LogError($"没有找到YIUIYooAssetSetting");
+                        return;
+                    }
+
+                    EditorUtility.SetDirty(yiuiSetting);
+                    tempSetting.Packages = yiuiSetting.Packages;
+                    fieldInfo.SetValue(null, tempSetting);
+                }
+                else
+                {
+                    fieldInfo.SetValue(null, null);
+                }
+
+                CloseWindowRefresh();
+                EditorApplication.ExecuteMenuItem("YooAsset/AssetBundle Collector");
+            }
+            else
+            {
+                Debug.LogError($"没有找到AssetBundleCollectorSettingData._setting");
+            }
+        }
+
+        private void CreateUIProjectRes()
+        {
+            var path = $"{Application.dataPath}/../{UIProjectResPath}";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
         }
     }
 }
